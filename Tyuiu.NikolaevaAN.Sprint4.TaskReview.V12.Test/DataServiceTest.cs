3d27850f772d060b7210f5025980d1adc4966b02
Tyuiu.NikolaevaAN@outlook.com <Tyuiu.NikolaevaAN@outlook.com>
2023-11-14 14:31:04 +0500

    Add project files.

---
+++ Tyuiu.NikolaevaAN.Sprint4.TaskReview.V12.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.NikolaevaAN.Sprint4.TaskReview.V12.Lib;
+
+namespace Tyuiu.NikolaevaAN.Sprint4.TaskReview.V12.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+            int rows = 3;
+            int columns = 4;
+            int[,] mtrx = new int[rows, columns];
+            string str = "658932125478";
+            int res = ds.Calculate(rows, columns, str);
+            int wait = 30;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.NikolaevaAN.Sprint4.TaskReview.V12/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.NikolaevaAN.Sprint4.TaskReview.V12.Lib;
+
+namespace Tyuiu.NikolaevaAN.Sprint4.TaskReview.V12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #4 | Выполнила: Николаева А. Н. | ПКТб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Задание по спринту (SprintReview) #4                              *");
+            Console.WriteLine("* Задание #TaskReview                                                     *");
+            Console.WriteLine("* Вариант #12                                                             *");
+            Console.WriteLine("* Выполнила: Николаева Александра Николаевна | ПКТб-23-2                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая преобразует строку в матрицу 3 на 4 и       *");
+            Console.WriteLine("* подсчитает сумму нечетных чисел.                                        *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int rows = 3;
+            int columns = 4;
+            int[,] mtrx = new int[rows, columns];
+            string str = "658932125478";
+
+            int index = 0;
+            Console.WriteLine("\nМассив:");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{str[index]}\t");
+                    index++;
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int res = ds.Calculate(rows, columns, str);
+            Console.WriteLine("Сумма нечётных чисел = " + res);
+            Console.ReadKey();
+        }
+    }
+}
